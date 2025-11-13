3556753da441910fb8d0ab9cd3849158464587b2
Eduardo Carvalho <propagno@gmail.com>
2025-11-13 18:28:42 -0300

    fix: adiciona H2 para testes e simplifica OpenApiConfigTest

--- src/test/java/br/com/propagno/falecomjesus/adapter/input/rest/config/OpenApiConfigTest.java
+++ src/test/java/br/com/propagno/falecomjesus/adapter/input/rest/config/OpenApiConfigTest.java
@@ -2,18 +2,10 @@ package br.com.propagno.falecomjesus.adapter.input.rest.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
-@ActiveProfiles("test")
 class OpenApiConfigTest {
-
-    @Autowired(required = false)
-    private OpenAPI openAPI;
 
     @Test
     void shouldLoadOpenApiConfig() {
@@ -31,6 +23,13 @@ class OpenApiConfigTest {
         assertNotNull(api.getInfo());
         assertEquals("Fale com Jesus API", api.getInfo().getTitle());
         assertEquals("1.0.0", api.getInfo().getVersion());
+        assertNotNull(api.getInfo().getContact());
+        assertEquals("Equipe de Desenvolvimento", api.getInfo().getContact().getName());
+        assertEquals("dev@propagno.com.br", api.getInfo().getContact().getEmail());
+        assertNotNull(api.getInfo().getLicense());
+        assertEquals("Proprietary", api.getInfo().getLicense().getName());
+        assertNotNull(api.getServers());
+        assertEquals(3, api.getServers().size());
     }
 }
 
